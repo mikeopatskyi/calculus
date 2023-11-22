4e96f27b86a21f6ca621bec60f1ba02721eb86a8
Mike Opatskyi <michael@seek.pe>
2023-11-22 12:27:21 +0200

    MobX - refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
 import 'package:calculus/screens/home_screen.dart';
-import 'package:calculus/store/calculator_store.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
 
   runApp(const CalculusApp());
-  // runApp(
-  //   MultiProvider(
-  //     providers: [
-  //       Provider<CalculatorStore>(
-  //         create: (_) => CalculatorStore(),
-  //       ),
-  //     ],
-  //     child: const CalculusApp(),
-  //   ),
-  // );
 }
 
 class CalculusApp extends StatelessWidget {
